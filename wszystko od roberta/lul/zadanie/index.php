6b1f1ff03791bb73d88f5e4d060d6bb99752dc57
Asus-chan <maciekczechyr@gmail.com>
2017-05-17 11:56:49 +0200

    12
    12

---
+++ wszystko od roberta/lul/zadanie/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Rejestracja</title>
+<meta charset="utf-8" lang="pl">
+</head>
+<body>
+<?php
+<form action="show.php" method="post" />
+login:&nbsp;<input type="text" name="imie" value="" /><br /><br />
+nazwisko:&nbsp;<input type="text" name="nazwisko" value="" /><br /><br />
+login:&nbsp;<input type="text" name="login" value="" /><br /><br />
+hasło:&nbsp;<input type="password" name="haslo" value="" /><br /><br />
+mail:&nbsp;<input type="email" name="mail" value= "" /><br /><br />
+<input type="submit" value="Zarejestruj" />
+</form>
+?>
+	
+</body>
+</html>
